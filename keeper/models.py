0264254c699f6523acf40e88a4fd3e47dfb58784
Mikhail <medvedek2002@gmail.com>
2019-01-30 22:12:37 +0300

    CustomUser is created successfully

--- keeper/models.py
+++ keeper/models.py
@@ -5,6 +5,7 @@ from django.contrib.auth.models import AbstractUser
 
 
 class CustomUser(AbstractUser):
+    password = models.CharField(max_length=200)
     card_id = models.IntegerField(default=8000000000)
 
 
